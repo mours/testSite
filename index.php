a4ebb284dedcf91ec3d137d9fd8bf789440d0189
Clément <clement.marcel1@gmail.com>
2012-07-30 08:03:00 +0200

    add Mappy function

--- index.php
+++ index.php
@@ -11,4 +11,6 @@ if(isset($_SESSION['message'])) echo $_SESSION['message']; ?>
 <?php include('templates/add.tpl.php'); ?>
 <?php include('book.php'); ?>
 <br />
+
+
 <?php include('footer.php'); ?>
